2d8990731dabfbc282ca464e753680b710ca1665
chenBright <1021774709@qq.com>
2016-10-18 23:48:28 +0800

    fix bugs about about null and undefined

--- src/includes.js
+++ src/includes.js
@@ -3,8 +3,5 @@ import {makeStr} from './helper/makeStr'
 export let includes = (str, subStr) => {
   str = makeStr(str)
   subStr = makeStr(subStr)
-  if(str.length === 0 || subStr.length === 0) {
-    return false
-  }
   return !!(~str.indexOf(subStr))
 }
--- test/includes.test.js
+++ test/includes.test.js
@@ -5,7 +5,19 @@ describe('includes', () => {
   it('hello includes llo', () => {
     expect(includes('hello', 'llo')).to.be.true
   })
-  it('hello doesn\'t includes lle', () => {
+  it('hello doesn\'t include lle', () => {
     expect(includes('hello', 'lle')).to.be.false
+  })
+  it('1234  includes 12', () => {
+    expect(includes(1234, 12)).to.be.true
+  })
+  it('1234 doesn\'t include 56', () => {
+    expect(includes(1234, 56)).to.be.false
+  })
+  it('null includes empty string', () => {
+    expect(includes(null, '')).to.be.true
+  })
+  it('hello doesn\'t include 34', () => {
+    expect(includes(null, 34)).to.be.false
   })
 })
