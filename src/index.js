acc84388a65e76e5f58b9faf0fb68c639780a57d
chenBright <1021774709@qq.com>
2016-10-21 21:33:17 +0800

    entry

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import { capitalize } from './capitalize'
+import { chars } from './chars'
+import { clean } from './clean'
+import { count } from './count'
+import { decapitalize } from './decapitalize'
+import { includes } from './includes'
+import { insert } from './insert'
+import { isBlank } from './isBlank'
+import { join } from './join'
+import { ltrim } from './ltrim'
+import { pred } from './pred'
+import { replaceAll } from './replaceAll'
+import { reverse } from './reverse'
+import { rtrim } from './rtrim'
+import { succ } from './succ'
+import { swapCase } from './swapCase'
+import { trim } from './trim'
+
+export let _str = {
+  capitalize,
+  chars,
+  clean,
+  count,
+  decapitalize,
+  includes,
+  insert,
+  isBlank,
+  join,
+  ltrim,
+  pred,
+  replaceAll,
+  reverse,
+  rtrim,
+  succ,
+  swapCase,
+  trim
+}
