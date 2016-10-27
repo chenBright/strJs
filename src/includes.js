354f8060d67ea94235118c86bb233c77b2a831c6
chenBright <1021774709@qq.com>
2016-10-27 12:36:14 +0800

    update solution with ES6

--- src/includes.js
+++ src/includes.js
@@ -3,5 +3,5 @@ import { makeStr } from './helper/makeStr'
 export let includes = (str, subStr) => {
   str = makeStr(str)
   subStr = makeStr(subStr)
-  return !!(~str.indexOf(subStr))
+  return str.includes(subStr)
 }
